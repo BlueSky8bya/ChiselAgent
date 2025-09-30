ea5f9bd477a28a62e4a82fbee6b4d015b4467ddc
임상택 <blackspace665@gmail.com>
2025-09-30 23:23:59 +0900

    Initial commit

---
+++ src/client/java/whiteheaven/chiselagent/ChiselAgentClient.java
@@ -0,0 +1,10 @@
+package whiteheaven.chiselagent;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChiselAgentClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/whiteheaven/chiselagent/ChiselAgentDataGenerator.java
@@ -0,0 +1,11 @@
+package whiteheaven.chiselagent;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChiselAgentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
